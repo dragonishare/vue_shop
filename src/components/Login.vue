5b631e07b9078d5a10ddba5593a8afc4ede55ff5
Dragon <dragonishare@gmail.com>
2020-05-12 23:41:05 +0800

    整理文件

---
+++ src/components/Login.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="login-container">login</div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="less" scoped>
+/* .login-container {
+  background: #2b4b6b;
+} */
+</style>
