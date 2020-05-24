8c652e7207900e46d9aaf67c1b0e885912af0b3d
Dragon <dragonishare@gmail.com>
2020-05-24 22:31:06 +0800

    1、登录成功后设置token；2、路由导航守卫控制页面访问权限

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -26,6 +26,7 @@
             v-model="loginForm.password"
             prefix-icon="iconfont icon-3702mima"
             type="password"
+            @keyup.enter.native="login"
           ></el-input>
         </el-form-item>
         <!-- 按钮区域 -->
@@ -43,8 +44,8 @@ export default {
     return {
       // 这是登录表单的数据绑定对象
       loginForm: {
-        username: '',
-        password: ''
+        username: 'admin',
+        password: '123456'
       },
       // 这是表单的验证规则对象
       loginFormRules: {
@@ -73,6 +74,13 @@ export default {
         const { data: res } = await this.$http.post('login', this.loginForm)
         if (res.meta.status !== 200) return this.$message.error('登录失败')
         this.$message.success('登录成功')
+
+        // 1. 将登录成功之后的 token，保存到客户端的 sessionStorage 中
+        //   1.1 项目中除了登录之外的其他API接口，必须在登录之后才能访问
+        //   1.2 token 只应在当前网站打开期间生效，所以将 token 保存在 sessionStorage 中
+        window.sessionStorage.setItem('token', res.data.token)
+        // 2. 通过编程式导航跳转到后台主页，路由地址是 /home
+        this.$router.push('/home')
       })
     }
   }
