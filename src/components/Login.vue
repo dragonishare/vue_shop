10962480b4e590bffeae58c38cff5898600bbda8
Dragon <dragonishare@gmail.com>
2020-05-24 11:02:23 +0800

    实现表单的重置功能

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -7,6 +7,7 @@
       </div>
       <!-- 登录表单区域 -->
       <el-form
+        ref="loginFormRef"
         :model="loginForm"
         :rules="loginFormRules"
         label-width="0px"
@@ -30,7 +31,7 @@
         <!-- 按钮区域 -->
         <el-form-item class="btns">
           <el-button type="primary">登录</el-button>
-          <el-button type="info">重置</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -58,6 +59,12 @@ export default {
           { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
         ]
       }
+    }
+  },
+  methods: {
+    // 点击重置按钮，重置登录表单
+    resetLoginForm() {
+      this.$refs.loginFormRef.resetFields()
     }
   }
 }
