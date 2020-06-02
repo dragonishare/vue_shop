fc5f0e88611fc8e4cb4ae2bf7380dce61237f63c
Dragon <dragonishare@gmail.com>
2020-06-02 22:25:19 +0800

    实现基本的主页布局和header布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,21 @@
 <template>
-  <div><el-button type="info" @click="logout">退出</el-button></div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div><span>电商后台管理系统</span></div>
+      <el-button
+        type="info"
+        @click="logout"
+      >退出</el-button>
+    </el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">Aside</el-aside>
+      <!-- 右侧内容主体 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 <script>
 export default {
@@ -12,5 +28,25 @@ export default {
 }
 </script>
 <style lang="less" scoped>
-
+.home-container {
+  height: 100%;
+}
+.el-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 20px;
+  color: #fff;
+  background-color: #373d41;
+  > div {
+    display: flex;
+    align-items: center;
+  }
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background-color: #eaedf1;
+}
 </style>
