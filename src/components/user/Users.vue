140e994d4b61af3addb8b9dd8678e6cc4045919c
Dragon <dragonishare@gmail.com>
2020-06-07 22:31:59 +0800

    feat:用户列表-通过路由的形式展示用户列表组件

---
+++ src/components/user/Users.vue
@@ -0,0 +1,3 @@
+<template>
+  <div><h3>用户列表组件</h3></div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
 import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/Users.vue'
 
 Vue.use(VueRouter)
 
@@ -18,6 +19,10 @@ const routes = [
       {
         path: '/welcome',
         component: Welcome
+      },
+      {
+        path: '/users',
+        component: Users
       }
     ]
   }
