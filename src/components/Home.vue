46ded22709c75854a71a33b8699ad1e0a35a3bc0
Dragon <dragonishare@gmail.com>
2020-06-07 22:20:23 +0800

    实现侧边栏路由链接的改造

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -24,6 +24,7 @@
           unique-opened
           :collapse="isCollapse"
           :collapse-transition="false"
+          router
         >
           <!-- 一级菜单 -->
           <el-submenu
@@ -40,7 +41,7 @@
             </template>
             <!-- 二级菜单 -->
             <el-menu-item
-              :index="`${subItem.id}`"
+              :index="`/${subItem.path}`"
               v-for="subItem in item.children"
               :key="subItem.id"
             >
