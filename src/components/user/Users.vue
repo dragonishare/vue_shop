2cc42af504251beb9182db2381a3da0947e578f4
Dragon <dragonishare@gmail.com>
2020-06-08 00:31:43 +0800

    feat:用户列表-实现分页效果

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -42,6 +42,8 @@
           </el-tooltip>
         </el-table-column>
       </el-table>
+      <!-- 分页区域 -->
+      <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="queryInfo.pagenum" :page-sizes="[1, 2, 5, 10]" :page-size="queryInfo.pagesize" layout="total, sizes, prev, pager, next, jumper" :total="total"></el-pagination>
     </el-card>
   </div>
 </template>
@@ -52,7 +54,9 @@ export default {
       // 获取用户列表的参数对象
       queryInfo: {
         query: '',
+        // 当前的页数
         pagenum: 1,
+        // 当前每页显示多少条数据
         pagesize: 2
       },
       userList: [],
@@ -70,6 +74,18 @@ export default {
       }
       this.userList = res.data.users
       this.total = res.data.total
+    },
+    // 监听 pageSize 改变的事件
+    handleSizeChange(newSize) {
+      // console.log(newSize)
+      this.queryInfo.pagesize = newSize
+      this.getUserList()
+    },
+    // 监听 页码值 改变的事件
+    handleCurrentChange(newPage) {
+      // console.log(newPage)
+      this.queryInfo.pagenum = newPage
+      this.getUserList()
     }
   }
 }
