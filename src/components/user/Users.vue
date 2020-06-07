a755fe5a0b1c5551399a78971bad44b02ecba191
Dragon <dragonishare@gmail.com>
2020-06-08 00:16:19 +0800

    feat:用户列表-通过作用域插槽渲染操作列

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -31,7 +31,16 @@
             <el-switch v-model="scope.row.mg_state"></el-switch>
           </template>
         </el-table-column>
-        <el-table-column label="操作"></el-table-column>
+        <el-table-column label="操作" width="180px">
+          <!-- 修改按钮 -->
+          <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+          <!-- 删除按钮 -->
+          <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+          <!-- 分配角色按钮 -->
+          <el-tooltip effect="dark" content="分配角色" placement="top" :enterable="false">
+            <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
+          </el-tooltip>
+        </el-table-column>
       </el-table>
     </el-card>
   </div>
